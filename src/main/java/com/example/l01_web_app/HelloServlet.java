16afa025d6f01d86ea6fe42e3ffca91af8dd8c89
kyoungmin <develckm86@gmail.com>
2025-10-22 16:51:44 +0900

    first commit

---
+++ src/main/java/com/example/l01_web_app/HelloServlet.java
@@ -0,0 +1,42 @@
+package com.example.l01_web_app;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+
+
+//http://localhost:8080/l01_web_app_war_exploded/
+
+//리소스 : 동적 ,정적
+//동적리소스 : 프로그램언어(java,js,c#,c)로 작성되어서,DB 접속(JDBC,Dao,Service)
+//정적리소스 : html,css,js(프론트에서 실행)
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello Web Application";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + (1+3+12)*19 + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %>
+</h1>
+<br/>
+<a href="hello-servlet">안녕하세요!! 웹어플리케이션입니다.</a>
+<%--/Users/som/Downloads/apache-tomcat-11.0.13/bin
+/Users/som/Downloads/apache-tomcat-11.0.13/bin/catalina.sh
+
+sudo chmod +x catalina.sh
+
+
+sudo chown -R $(whoami) /Users/som/Downloads/apache-tomcat-10.1.48
+
+--%>
+</body>
+</html>
